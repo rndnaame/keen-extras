373234e8386bbd7e055a6a7e48ccecabd47bd17d
Anonymous <grabik.pk@gmail.com>
2026-04-09 03:40:50 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+REPO="keen-extras"          # ← поменяй на имя своего репозитория, если другое
+SCRIPT="keenextras.sh"
+TMP_DIR="/tmp"
+OPT_DIR="/opt"
+BRANCH="main"
+
+print_message() {
+  local message="$1"
+  local color="${2:-$NC}"
+  local border=$(printf '%0.s-' $(seq 1 $((${#message} + 2))))
+  printf "${color}\n+${border}+\n| ${message} |\n+${border}+\n${NC}\n"
+}
+
+packages_checker() {
+  local missing=""
+  for pkg in "$@"; do
+    if ! opkg list-installed | grep -q "^$pkg"; then
+      missing="$missing $pkg"
+    fi
+  done
+  if [ -n "$missing" ]; then
+    print_message "Устанавливаем:$missing" "\033[1;33m"
+    opkg update >/dev/null 2>&1
+    opkg install $missing
+    echo ""
+  fi
+}
+
+packages_checker curl tar findutils jq
+
+curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/$BRANCH/$SCRIPT" --output $TMP_DIR/$SCRIPT
+mv "$TMP_DIR/$SCRIPT" "$OPT_DIR/$SCRIPT"
+chmod +x $OPT_DIR/$SCRIPT
+
+cd $OPT_DIR/bin
+ln -sf $OPT_DIR/$SCRIPT $OPT_DIR/bin/keenextras
+
+print_message "Установка завершена! Запуск: keenextras" "\033[1;32m"
+exec $OPT_DIR/$SCRIPT
